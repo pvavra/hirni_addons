31ec09952b98519819e450b43beddb2aa27d4ded
Peter Vavra <peter.vavra.1984@gmail.com>
2019-12-06 13:14:59 +0100

    BF: correctly references path of python scripts

--- procedures/repack-studyspec.sh
+++ procedures/repack-studyspec.sh
@@ -4,9 +4,12 @@
 set -e -u
 set -x
 
+
+folder=$(dirname "$0")
+
 if [ "$#" -gt 1 ]
 then
-    datalad run --explicit --input "$1" --output "$2" python helplers/repack-studyspec.py "$1" "$2"
+    datalad run --explicit --input "$1" --output "$2" python $folder/../helplers/repack_studyspec.py "$1" "$2"
 else
-    datalad run --explicit --input "$1" --output "$1" python helplers/repack-studyspec.py "$1" "$1"
+    datalad run --explicit --input "$1" --output "$1" python $folder/../helplers/repack_studyspec.py "$1" "$1"
 fi
--- procedures/unpack-studyspec.sh
+++ procedures/unpack-studyspec.sh
@@ -4,9 +4,10 @@
 set -e -u
 set -x
 
+folder=$(dirname "$0")
 if [ "$#" -gt 1 ]
 then
-    datalad run --explicit --input "$1" --output "$2" python helplers/unpack-studyspec.py "$1" "$2"
+    datalad run --explicit --input "$1" --output "$2" python $folder/../helpers/unpack_studyspec.py "$1" "$2"
 else
-    datalad run --explicit --input "$1" --output "$1" python helplers/unpack-studyspec.py "$1" "$1"
+    datalad run --explicit --input "$1" --output "$1" python $folder../helpers/unpack_studyspec.py "$1" "$1"
 fi
