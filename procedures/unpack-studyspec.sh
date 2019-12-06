9cc3c1c1219010b38bd3fffcacc532d61ce11e21
Peter Vavra <peter.vavra.1984@gmail.com>
2019-12-06 13:19:21 +0100

    BF: fix missing slash..

--- procedures/unpack-studyspec.sh
+++ procedures/unpack-studyspec.sh
@@ -9,5 +9,5 @@ if [ "$#" -gt 1 ]
 then
     datalad run --explicit --input "$1" --output "$2" python $folder/../helpers/unpack_studyspec.py "$1" "$2"
 else
-    datalad run --explicit --input "$1" --output "$1" python $folder../helpers/unpack_studyspec.py "$1" "$1"
+    datalad run --explicit --input "$1" --output "$1" python $folder/../helpers/unpack_studyspec.py "$1" "$1"
 fi
