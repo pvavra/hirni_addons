b06f31991a9761fc64ab7fdd8ec68092a4da42f4
Peter Vavra <peter.vavra.1984@gmail.com>
2020-01-17 01:02:13 +0100

    minor improvements

--- procedures/behavior-converter.sh
+++ procedures/behavior-converter.sh
@@ -18,8 +18,11 @@ call="converter_behavior($file_input, $subject, $ds, $session)"
 
 # ds="$1"
 
+pushd "$dir_converter"
+matlab -nodisplay -nosplash -singleCompThread -r "$call; exit"
+popd
 
-  pushd "$dir_converter"
-  matlab -nojvm -nodisplay -nosplash -singleCompThread -r "$call; exit"
-  popd
 # fi
+# TODO: use datalad run with input/output defs to avoid saving more than
+#  created by the current procedure
+datalad save -m "added events.tsv files for sub-$subject/ses-$session"
